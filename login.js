cb37883b3f8a10ae19c4963b1a04c83e8a2efa34
Bishan-Pankaja <bishanpankaja4@gmail.com>
2024-10-26 02:56:28 -0700

    Update login.js

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   
     // Array of 50 users with usernames and passwords
     const users = [
-      { username: "user1", password: "password1" },
+      { username: "1", password: "1" },
       { username: "user2", password: "password2" },
       { username: "user3", password: "password3" },
       { username: "user4", password: "password4" },
