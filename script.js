436420dda5e6a7b38bbf300d077e6b6b980baaeb
Bishan-Pankaja <bishanpankaja4@gmail.com>
2024-10-26 02:49:05 -0700

    4

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function openPopup(imageSrc, description) {
-    document.getElementById('popup-image').src = imageSrc;
-    document.getElementById('popup-description').innerText = description;
-    document.getElementById('popup').style.display = 'flex';
-}
-
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
-
+AOS.init({
+    duration: 1000
+  });
+  
